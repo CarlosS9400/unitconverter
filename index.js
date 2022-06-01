0e1f04b05a370276300b6c5bb1d0faa8da296228
CarlosS9400 <100578104+CarlosS9400@users.noreply.github.com>
2022-06-01 21:11:21 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let lengthEl = document.getElementById("length-el");
 let volumnEl = document.getElementById("volume-el");
 let massEl = document.getElementById("mass-el");
+let errorEl = document.getElementById("error-el");
 
 let inputEl = document.getElementById("input-el");
 
@@ -14,37 +15,50 @@ let kilograms;
 let pounds;
 
 
-function convertUnit() {
+function convert() {
+    
 
+    if (inputEl.value.length == 0) {
+
+        let error ="You entered nothing"
+        errorEl.textContent=error
+        lengthEl.textContent=""
+        volumnEl.textContent=""
+        massEl.textContent=""
+        
+       
+
+    }else{
  
-// meter/feet conversion
+    // meter/feet conversion
 
-feet = inputEl.value * 	3.280839895;
-meters = inputEl.value / 3.280839895;
+        feet = inputEl.value * 	3.280839895;
+        meters = inputEl.value / 3.280839895;
 
-lengthEl.textContent = inputEl.value + " meters = " + feet.toFixed(3) + " feet | " +
-                       inputEl.value + " feet = " + meters.toFixed(3) + " meters";
+        lengthEl.textContent = inputEl.value + " meters = " + feet.toFixed(3) + " feet | " +
+                            inputEl.value + " feet = " + meters.toFixed(3) + " meters";
 
-// liters/gallons conversion
+        // liters/gallons conversion
 
-liters = inputEl.value * 3.78541178;
-gallons = inputEl.value / 3.78541178;
+        liters = inputEl.value * 3.78541178;
+        gallons = inputEl.value / 3.78541178;
 
-volumnEl.textContent = inputEl.value + " liters = " + gallons.toFixed(3) + " gallons   | " +
-inputEl.value + " gallons = " + liters.toFixed(3) + " leters";
+        volumnEl.textContent = inputEl.value + " liters = " + gallons.toFixed(3) + " gallons   | " +
+        inputEl.value + " gallons = " + liters.toFixed(3) + " leters";
 
-// kilograms / pounds conversion
+        // kilograms / pounds conversion
 
-pounds = inputEl.value * 2.20462262;
-kilograms = inputEl.value / 2.20462262;
+        pounds = inputEl.value * 2.20462262;
+        kilograms = inputEl.value / 2.20462262;
 
-massEl.textContent =  inputEl.value + " kilograms = " + pounds.toFixed(3) + " pounds   | " + inputEl.value + " pounds = " + kilograms.toFixed(3) + " kilogramss";
-
-
+        massEl.textContent =  inputEl.value + " kilograms = " + pounds.toFixed(3) + " pounds   | " + inputEl.value + " pounds = " + kilograms.toFixed(3) + " kilogramss";
 
 
 
 
+
+
+    }
 }
   
 
